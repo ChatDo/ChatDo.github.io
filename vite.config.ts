ebe1c74cde44938800d37291279d1558b6c935ab
ChatDo <nicolas.julie@epitech.eu>
2025-01-07 13:05:46 +0100

    valentine page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
-import UnoCSS from 'unocss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const config: UserConfig = {
-	plugins: [UnoCSS(), sveltekit()]
-};
-
-export default config;
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
